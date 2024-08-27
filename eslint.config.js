0e1633d44dc1f94918daaeac6ff8f6322e1e5e95
Saikiran121 <saikiranbiradar76642@gmail.com>
2024-08-27 19:33:48 +0000

    Create eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Your ESLint configuration here
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    // Your custom rules here
+  },
+};
