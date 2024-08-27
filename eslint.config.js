6930058bc92d378c8336b970f4580e61aa1cff87
Saikiran121 <saikiranbiradar76642@gmail.com>
2024-08-27 19:42:57 +0000

    Final changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,26 @@
-module.exports = {
-  languageOptions: {
-    globals: {
-      node: true,
+module.exports = [
+  {
+    languageOptions: {
+      globals: {
+        node: true,
+      },
+    },
+    rules: {
+      // Your custom rules here
     },
   },
-  extends: 'eslint:recommended',
-  rules: {
-    // Your custom rules here
+  {
+    // eslint:recommended config
+    env: {
+      es2021: true,
+    },
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-console': 'off',
+      'no-debugger': 'off',
+    },
   },
-};
+];
